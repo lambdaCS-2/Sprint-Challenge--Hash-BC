1e7705107039deeb2eb1bb478d7fb0fd02f1f216
sriley <sarahriley.dev@gmail.com>
2020-02-14 14:47:19 -0500

    mvp

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -7,7 +7,7 @@ from uuid import uuid4
 
 from timeit import default_timer as timer
 
-import random
+from random import random
 
 
 def proof_of_work(last_proof):
@@ -25,6 +25,10 @@ def proof_of_work(last_proof):
     print("Searching for next proof")
     proof = 0
     #  TODO: Your code here
+    block_string = hashlib.sha256(f"{last_proof}".encode()).hexdigest()
+    proof = last_proof
+    while not valid_proof(block_string, proof):
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -40,7 +44,10 @@ def valid_proof(last_hash, proof):
     """
 
     # TODO: Your code here!
-    pass
+    guess = str(proof).encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:6] == last_hash[-6:]
+    
 
 
 if __name__ == '__main__':
@@ -48,7 +55,7 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         node = sys.argv[1]
     else:
-        node = "https://lambda-coin.herokuapp.com/api"
+        node = "https://lambda-coin-test-1.herokuapp.com/api"
 
     coins_mined = 0
 
